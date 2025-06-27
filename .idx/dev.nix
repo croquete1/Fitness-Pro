4f2b129ea055eace2bbc54d104fba89074d6d5ea
croquete1.andre <croquete1.andre@gmail.com>
2025-06-27 13:50:29 +0000

    update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,11 +5,12 @@
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
-  packages = [
+  packages = [ pkgs.yarn
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
+
     # pkgs.nodePackages.nodemon
   ];
 
