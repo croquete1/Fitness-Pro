71e4a466c334e03a90e04c0afb813c188dc1c306
André Gomes <andre.capoeira1@hotmail.com>
2025-07-03 18:02:34 +0100

    update

---
+++ start_dashboard.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Abrindo projeto React/Vite...
+
+:: Navegar para a pasta do projeto (ajuste conforme necessário)
+cd /d %~dp0react_vite_dashboard_full
+
+:: Instalar dependências (apenas na 1ª vez)
+if not exist node_modules (
+  echo Instalando dependências...
+  npm install
+)
+
+:: Iniciar servidor de desenvolvimento
+echo Iniciando o servidor...
+npm run dev
+
+pause
