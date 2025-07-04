715a807e44877daf45a23fdd9ec02ad4215ee38f
André Gomes <andre.capoeira1@hotmail.com>
2025-07-04 10:00:37 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
-// PLACEHOLDER: conteúdo real será inserido no próximo passo
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./pages/LoginForm";
+import { AdminDashboard, TrainerDashboard, ClienteDashboard } from "./pages/RoleBasedPages";
+import { AuthRoleProvider } from "./contexts/authRoleContext";
+import { AdminOnly, TrainerOnly, ClienteOnly } from "./pages/ProtectedRoutes";
+import RedirectByRole from "./pages/RedirectByRole";
+
+export default function App() {
+  return (
+    <AuthRoleProvider>
+      <Routes>
+        <Route path="/" element={<RedirectByRole />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/admin" element={<AdminOnly><AdminDashboard /></AdminOnly>} />
+        <Route path="/trainer" element={<TrainerOnly><TrainerDashboard /></TrainerOnly>} />
+        <Route path="/cliente" element={<ClienteOnly><ClienteDashboard /></ClienteOnly>} />
+      </Routes>
+    </AuthRoleProvider>
+  );
+}
