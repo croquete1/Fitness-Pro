20877f8cced77c323f6b1614ae53868c00b55e8c
André Gomes <andre.capoeira1@hotmail.com>
2025-07-04 21:21:30 +0100

    update

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+// src/firebase/firebase.js
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+// ⚠️ Substitui estes valores pelos teus do Firebase Console
+const firebaseConfig = {
+  apiKey: 'AIzaSyAcGGE37ZiTFcz8mo1pSECvDCDOXdzbSHY',
+  authDomain: 'fitness-pro-12345.firebaseapp.com',
+  projectId: 'fitness-pro-12345',
+  storageBucket: 'fitness-pro-12345.appspot.com',
+  messagingSenderId: '123456789012',
+  appId: '1:123456789012:web:abcdefghijklmnopqrstuvwxyz'
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
