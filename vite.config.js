cccaefd49b7fea271046e4ce2763a27eccf9b266
André Gomes <andre.capoeira1@hotmail.com>
2025-07-05 18:00:14 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+// vite.config.js
 
-export default defineConfig({
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
+
+module.exports = defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`
-    }
-    export default defineConfig({
-  ...
   build: {
+    chunkSizeWarningLimit: 600, // aumenta o limite do warning
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          recharts: ['recharts'],
-        }
-      }
-    }
-  }
-})
-
-  }
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
+      },
+    },
+  },
 })
