ce260ebc017ea5610c130c889f187a8bd9b9595f
André Gomes <andre.capoeira1@hotmail.com>
2025-07-05 20:02:50 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthRoleProvider } from './contexts/authRoleContext'
+import { AppRoutes } from './router/routes'
+
+<AppRoutes />
+
 
 import './index.css'
 
