6029c32be1cbfb250b6fceca28c6f0b267798524
André Gomes <andre.capoeira1@hotmail.com>
2025-07-05 19:36:56 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthRoleProvider } from './contexts/authRoleContext'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthRoleProvider>
+        <App />
+      </AuthRoleProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
