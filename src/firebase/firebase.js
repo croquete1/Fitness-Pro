4ac88d32daf3644d700c7b1ebca3a5db9e65aab0
André Gomes <andre.capoeira1@hotmail.com>
2025-07-05 18:41:33 +0100

    update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,3 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+export async function requestPermission() {
+  const permission = await Notification.requestPermission()
+  return permission
+}
