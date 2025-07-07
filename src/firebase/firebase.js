5b0f4dc6c107fd6ba4b306a48871129a7dc35307
André Gomes <andre.capoeira1@hotmail.com>
2025-07-07 16:17:05 +0100

    update

--- src/firebase/firebase.js
+++
@@ -1,1 +0,0 @@
-// Firebase config with API keys
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcGGE37ZiTFcz8mo1pSECvDCDOXdzbSHY",
+  authDomain: "fitnesspro-36d8b.firebaseapp.com",
+  projectId: "fitnesspro-36d8b",
+  storageBucket: "fitnesspro-36d8b.appspot.com",
+  messagingSenderId: "881881913799",
+  appId: "1:881881913799:web:08399068acd96e83f0e1e2",
+  measurementId: "G-G3XJ0741CW"
+};
