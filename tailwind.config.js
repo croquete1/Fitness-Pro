861c28eaeb689cc42a36e8a938c4462b70877185
André Gomes <andre.capoeira1@hotmail.com>
2025-07-08 12:13:53 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,9 @@
-// Tailwind config
+// tailwind.config.js
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
