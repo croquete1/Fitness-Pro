8f205f1fd399452f82e1d3c237819cf7dd7e206a
André Gomes <andre.capoeira1@hotmail.com>
2025-07-10 18:06:20 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 // tailwind.config.js
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // correto
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          50: "#fafafa",
+          100: "#f5f5f5",
+          200: "#e5e5e5",
+          300: "#d4d4d4",
+          400: "#a3a3a3",
+          500: "#737373",
+          600: "#525252",
+          700: "#404040",
+          800: "#262626",
+          900: "#171717",
+        },
+      },
+    },
   },
   plugins: [],
 }
