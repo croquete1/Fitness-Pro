527c7948f87bc1502e4c9d5026a62ed7a9ab4280
André Gomes <andre.capoeira1@hotmail.com>
2025-07-10 15:58:23 +0100

    big update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,19 +1,23 @@
+
 // src/firebase/firebase.js
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getMessaging } from 'firebase/messaging'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAcGGE37ZiTFcz8mo1pSECvDCDOXdzbSHY",
-  authDomain: "fitness-pro-12345.firebaseapp.com",
-  projectId: "fitness-pro-12345",
-  storageBucket: "fitness-pro-12345.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456",
-  measurementId: "G-1234567890"
-}
+  authDomain: "fitnesspro-36d8b.firebaseapp.com",
+  projectId: "fitnesspro-36d8b",
+  storageBucket: "fitnesspro-36d8b.appspot.com",
+  messagingSenderId: "881881913799",
+  appId: "1:881881913799:web:08399068acd96e83f0e1e2",
+  measurementId: "G-G3XJ0741CW"
+};
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const messaging = getMessaging(app);
 
-export const db = getFirestore(app)
-export const messaging = getMessaging(app)
+export { auth, db, messaging, getToken, onMessage };
