831d8e3dff739c0bbe2dda4e900754d47ac133ad
André Gomes <andre.capoeira1@hotmail.com>
2025-07-11 15:00:53 +0100

    update

---
+++ build.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.join(__dirname, 'src');
+const distDir = path.join(__dirname, 'dist');
+
+function copyRecursiveSync(src, dest) {
+  if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
+  fs.readdirSync(src).forEach((item) => {
+    const srcPath = path.join(src, item);
+    const destPath = path.join(dest, item);
+    if (fs.statSync(srcPath).isDirectory()) {
+      copyRecursiveSync(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  });
+}
+
+// criar dist e copiar tudo de src para dist
+if (!fs.existsSync(distDir)) fs.mkdirSync(distDir);
+copyRecursiveSync(srcDir, distDir);
+console.log('Build concluída com sucesso!');
