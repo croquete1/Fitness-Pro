acd5785a6d01c9c77e4fbdf030e29975eb1fa400
André Gomes <andre.capoeira1@hotmail.com>
2025-07-11 10:10:44 +0100

    update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER",
-  appId: "YOUR_APP"
+  apiKey: "AIzaSyAcGGE37ZiTFcz8mo1pSECvDCDOXdzbSHY",
+  authDomain: "fitness-pro-12345.firebaseapp.com",
+  projectId: "fitness-pro-12345",
+  storageBucket: "fitness-pro-12345.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:0987654321abcdef"
 };
 
 const app = initializeApp(firebaseConfig);
