a1236771a6ce466b4f34dab1bb0c45592a7ae3e8
André Gomes <andre.capoeira1@hotmail.com>
2025-07-11 21:17:52 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
+  base: './', // importante para Firebase Hosting
   plugins: [react()],
   build: {
     outDir: 'dist'
