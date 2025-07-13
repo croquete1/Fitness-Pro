4328dfeaba17829858e24d3a74b3ea18c0ec6bc1
André Gomes <andre.capoeira1@hotmail.com>
2025-07-13 12:44:41 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
+import React, { lazy, useEffect } from 'react'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { themeChange } from 'theme-change'
+import checkAuth from './app/auth';
+import initializeApp from './app/init';
 
-import React from "react";
+// Importing pages
+const Layout = lazy(() => import('./containers/Layout'))
+const Login = lazy(() => import('./pages/Login'))
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+const Register = lazy(() => import('./pages/Register'))
+const Documentation = lazy(() => import('./pages/Documentation'))
+
+
+// Initializing different libraries
+initializeApp()
+
+
+// Check for login and initialize axios
+const token = checkAuth()
+
 
 function App() {
+
+  useEffect(() => {
+    // 👆 daisy UI themes initialization
+    themeChange(false)
+  }, [])
+
+
   return (
-    <div className="p-10 text-center">
-      <h1 className="text-4xl font-bold text-primary">Fitness Pro - DaisyUI</h1>
-      <p className="mt-4">Dashboard Inicial com tema alternável</p>
-    </div>
-  );
+    <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/documentation" element={<Documentation />} />
+          
+          {/* Place new routes over this */}
+          <Route path="/app/*" element={<Layout />} />
+
+          <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/>
+
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
