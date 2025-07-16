445962c5bded674dd531edd43c01815f9148e4c7
André Gomes <andre.capoeira1@hotmail.com>
2025-07-16 13:58:41 +0100

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
+// tailwind.config.js
 module.exports = {
-  content: ["index.html", "src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "index.html",
+    "src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
