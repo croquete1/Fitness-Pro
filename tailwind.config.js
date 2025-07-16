26db7467d4ab82b95ac98ae260becb664f015a34
André Gomes <andre.capoeira1@hotmail.com>
2025-07-16 16:41:22 +0100

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "index.html",
-    "src/**/*.{js,jsx,ts,tsx}"
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/horizon-tailwind-react/**/*.{js,jsx,ts,tsx}' // se usares Horizon UI
   ],
   theme: {
     extend: {},
