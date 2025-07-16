633dac50630213a1bc3f32968646c3e2df273a0c
André Gomes <andre.capoeira1@hotmail.com>
2025-07-16 10:21:08 +0100

    ssss

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,16 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const config = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  // outros campos se necessário…
+const firebaseConfig = {
+  apiKey: "AIzaSyAcGGE37ZiTFcz8mo1pSECvDCDOXdzbSHY",
+  authDomain: "fitnesspro-36d8b.firebaseapp.com",
+  projectId: "fitnesspro-36d8b",
+  storageBucket: "fitnesspro-36d8b.firebasestorage.app",
+  messagingSenderId: "881881913799",
+  appId: "1:881881913799:web:08399068acd96e83f0e1e2",
+  measurementId: "G-G3XJ0741CW"
 };
 
-const app = initializeApp(config);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
