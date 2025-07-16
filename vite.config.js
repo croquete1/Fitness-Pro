c210bd0ad4ccb870aeef6f1c370c21f61c8d979d
André Gomes <andre.capoeira1@hotmail.com>
2025-07-16 13:47:08 +0100

    ddd

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  // Sem configuração de root personalizada
+  base: "/",
+  plugins: [react()]
 });
