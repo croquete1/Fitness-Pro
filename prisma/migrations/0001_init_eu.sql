44c80adcf1b0f4f0295a1c8e04e67099e0ad163d
André Gomes <andre.capoeira1@hotmail.com>
2025-08-09 14:25:30 +0100

    asd

---
+++ prisma/migrations/0001_init_eu.sql
@@ -0,0 +1,25 @@
+﻿-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('cliente', 'pt', 'admin');
+
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "name" TEXT,
+    "role" "public"."Role" NOT NULL DEFAULT 'cliente',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_role_idx" ON "public"."users"("role");
+
