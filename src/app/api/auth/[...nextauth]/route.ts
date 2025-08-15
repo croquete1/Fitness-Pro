19d5a274d0f4e63fdc7f5c1ac6daf7b5802bff4c
André Gomes <andre.capoeira1@hotmail.com>
2025-08-15 23:51:43 +0100

    eee

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,10 @@
 // src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import authOptions from "@/lib/auth";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
