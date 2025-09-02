b434c49171021e0bb27ce37ed6c143a2ae53b931
André Gomes <andre.capoeira1@hotmail.com>
2025-09-02 22:29:29 +0100

    Update DashboardFrame.tsx

--- src/components/layout/DashboardFrame.tsx
+++ src/components/layout/DashboardFrame.tsx
@@ -1,17 +1,19 @@
-// src/components/layout/DashboardFrame.tsx (excerto)
-import React from "react";
-import SidebarProvider from "./SidebarProvider"; // default OK (também podes usar { SidebarProvider })
-import Sidebar from "./SidebarAdmin";
-import AppHeader from "./AppHeader";
+// src/components/layout/DashboardFrame.tsx (exemplo)
+'use client';
+
+import React from 'react';
+import SidebarAdmin from './SidebarAdmin';
+import SidebarProvider from './SidebarProvider'; // default export também disponível
+import AppHeader from './AppHeader';
 
 export default function DashboardFrame({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="app-shell">
-        <Sidebar />
-        <div className="app-main">
+      <div className="flex">
+        <SidebarAdmin />
+        <div className="flex-1 min-w-0">
           <AppHeader />
-          <main>{children}</main>
+          <main className="p-4 sm:p-6 md:p-8">{children}</main>
         </div>
       </div>
     </SidebarProvider>
